97847822cd38a72bf08eb5c2e7d1e83caff3a3d2
Dan Hermann <dan.hermann@elastic.co>
2019-01-22 15:14:14 -0600

    # This is a combination of 2 commits. # This is the 1st commit message:
    Update filter plugin for beta version of the API
    
    # This is the commit message #2:
    
    start of gradle wrappers for packaging plugin as a Ruby gem

--- src/test/java/org/logstash/javaapi/JavaFilterExampleTest.java
+++ src/test/java/org/logstash/javaapi/JavaFilterExampleTest.java
@@ -2,27 +2,47 @@ package org.logstash.javaapi;
 
 import co.elastic.logstash.api.Configuration;
 import co.elastic.logstash.api.Context;
+import co.elastic.logstash.api.Event;
+import co.elastic.logstash.api.FilterMatchListener;
 import org.junit.Assert;
 import org.junit.Test;
-import org.logstash.Event;
+import org.logstash.plugins.ConfigurationImpl;
+import org.logstash.plugins.ContextImpl;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class JavaFilterExampleTest {
 
     @Test
     public void testJavaExampleFilter() {
         String sourceField = "foo";
-        Configuration config = new Configuration(Collections.singletonMap("source", sourceField));
-        Context context = new Context();
-        JavaFilterExample filter = new JavaFilterExample(config, context);
+        Configuration config = new ConfigurationImpl(Collections.singletonMap("source", sourceField));
+        Context context = new ContextImpl(null);
+        JavaFilterExample filter = new JavaFilterExample("test-id", config, context);
 
-        Event e = new Event();
+        Event e = new org.logstash.Event();
+        TestMatchListener matchListener = new TestMatchListener();
         e.setField(sourceField, "abcdef");
-        Collection<Event> results = filter.filter(Collections.singletonList(e));
+        Collection<Event> results = filter.filter(Collections.singletonList(e), matchListener);
 
         Assert.assertEquals(1, results.size());
         Assert.assertEquals("fedcba", e.getField(sourceField));
+        Assert.assertEquals(1, matchListener.getMatchCount());
+    }
+}
+
+class TestMatchListener implements FilterMatchListener {
+
+    private AtomicInteger matchCount = new AtomicInteger(0);
+
+    @Override
+    public void filterMatched(Event event) {
+        matchCount.incrementAndGet();
+    }
+
+    public int getMatchCount() {
+        return matchCount.get();
     }
 }
